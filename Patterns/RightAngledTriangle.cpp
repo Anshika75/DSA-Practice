c838d1917fe1427a23678cf0e27f9f3271121f15
Anshika <anshikathakur596@gmail.com>
2023-07-24 23:23:02 +0530

    pattern1 and 2 done

--- Patterns/RightAngledTriangle.cpp
+++ Patterns/RightAngledTriangle.cpp
@@ -8,7 +8,7 @@ int main()
     cout<<"Enter number of lines of pattern: ";
     cin >> n;
     for(i=0; i<n; i++){
-       for(j=0; j<n; j++){
+       for(j=0; j<=i; j++){
             cout<<"*";
         } 
         cout<<"\n";
