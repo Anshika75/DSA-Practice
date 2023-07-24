43a01731474ee0d952dc64c8b589a04bd86835e6
Anshika <anshikathakur596@gmail.com>
2023-07-24 23:26:26 +0530

    Create RightAnglesNumberPyramid.cpp

---
+++ Patterns/RightAnglesNumberPyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, k;
+    cout<<"Enter number of lines of pattern: ";
+    cin >> n;
+    for(i=0; i<n; i++){
+        k = 1;
+       for(j=0; j<=i; j++){
+            cout<<k;
+            k++;
+        } 
+        cout<<"\n";
+    }
+
+    return 0;
+}
