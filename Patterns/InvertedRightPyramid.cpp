cf0d6e74abefdf70b13ee48d91df263445162d97
Anshika <anshikathakur596@gmail.com>
2023-07-24 23:35:53 +0530

    Create InvertedRightPyramid.cpp

---
+++ Patterns/InvertedRightPyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+    cout<<"Enter number of lines of pattern: ";
+    cin >> n;
+    for(i=0; i<=n; i++){
+       for(j=0; j<=n-i; j++){
+            cout<<"*";
+        } 
+        cout<<"\n";
+    }
+
+    return 0;
+}
