1272c42575fbf4bc593aea8922155f6b1e9c29df
Anshika <anshikathakur596@gmail.com>
2023-07-27 13:20:22 +0530

    Create DiamondStarPattern.cpp

---
+++ Patterns/DiamondStarPattern.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, k;
+    cout << "Enter number of lines of pattern: ";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+
+        for (j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    for (i = n; i >= 1; --i)
+    {
+
+        for (j = 0; j < n - i; ++j)
+        {
+            cout << " ";
+        }
+        for (j = 0; j < 2 * i - 1; ++j)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
