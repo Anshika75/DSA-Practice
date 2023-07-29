fb20e25570a15d8d542570a391c4aea89b0f797f
Anshika <anshikathakur596@gmail.com>
2023-07-29 17:34:44 +0530

    Create AlphaRampPattern.cpp

---
+++ Patterns/AlphaRampPattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i,j;
+    char k;
+    cout<<"Enter number of lines of pattern: ";
+    cin >> n;
+    for(i=0; i<n; i++){
+        k='A'+i;
+        for(j=0; j<=i; j++){
+            cout<<k<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
